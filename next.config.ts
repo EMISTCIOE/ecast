4c8d6ff36e9ac79f5b773c3cfe5487396a9ac0a9
Abhishek Panthee <80570587+abhishekpanthee@users.noreply.github.com>
2024-12-21 18:50:15 +0545

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,43 +1,43 @@
+import path from 'path';
 import type { NextConfig } from 'next';
 
+// Add MDX support by using @next/mdx with custom config
 const nextConfig: NextConfig = {
-  // Enable static export
-  output: 'export',
-
-  // Optional: Add trailing slashes for clean URLs
-  trailingSlash: true,
-
-  // Disable React strict mode if unnecessary
   reactStrictMode: false,
-
-  // Disable image optimization for static export
   images: {
-    unoptimized: true, // Disable image optimization
     remotePatterns: [
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
-      { protocol: 'https', hostname: 'shotcan.com' },
-      { protocol: 'https', hostname: 'app.svgator.com' },
-      { protocol: 'https', hostname: 'cdn.svgator.com' },
-      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'shotcan.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'app.svgator.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.svgator.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      
+     
     ],
   },
-
-  // Add MDX file support as page extensions
-  pageExtensions: ['ts', 'tsx', 'mdx'],
-
-  // Extend Webpack configuration to handle MDX files
+  pageExtensions: ['ts', 'tsx', 'mdx'], // Add MDX file support as page extensions
   webpack(config) {
+    // Add MDX loader to handle .mdx files
     config.module.rules.push({
       test: /\.mdx$/,
       use: [
         {
-          loader: '@mdx-js/loader', // MDX loader
+          loader: '@mdx-js/loader', // Use MDX loader to process .mdx files
         },
       ],
     });
-
-    return config;
-  },
-};
-
-export default nextConfig;
